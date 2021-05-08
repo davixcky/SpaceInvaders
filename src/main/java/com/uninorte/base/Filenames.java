23374bba0ab15e56db3471cec0640f2c9d8df3fa
David Orozco <orozco.davidj02@gmail.com>
2021-05-08 11:33:46 -0500

    Using array of Filenames instead of multiple variables
    Grouping the same assets on arrays of paths in order to keep the scopes
    clean and be able to iterate over an iterable

--- src/main/java/com/uninorte/base/Filenames.java
+++ src/main/java/com/uninorte/base/Filenames.java
@@ -1,6 +1,14 @@
 package com.uninorte.base;
 
 public class Filenames {
-    public static String BACKGROUND1 = "/backgrounds/background_1.gif";
-    public static String BACKGROUND2 = "/backgrounds/background_2.png";
+    public static String[] BACKGROUND_IMAGES = new String[] {
+            "/backgrounds/background_1.gif",
+            "/backgrounds/background_2.png"
+    };
+
+    public static String[] BACKGROUND_MUSIC = new String[] {
+            "/sounds/background_DuaLipa.wav",
+            "/sounds/background_thunderstruck.wav",
+            "/sounds/imagine_dragons.wav"
+    };
 }
